698c6759dc1633a39d65b0a5f5a03859f6e38312
AntonAntton <ivanko.anton1408@gmail.com>
2025-01-22 12:16:09 -0400

    Update homework_2_1.py

--- Lesson 2/homework_2_1.py
+++ Lesson 2/homework_2_1.py
@@ -1,18 +1,15 @@
 def analyze_object(obj: object) -> None:
     """
     Аналізує об'єкт, виводячи його тип і перераховуючи його атрибути та методи з їх типами.
-
-    :param obj: Об'єкт, який потрібно проаналізувати.
-    :return: Нічого не повертає (None).
     """
     # Виводимо тип об'єкта
-    print(f"Тип об'єкта: {type(obj)}")
+    print(f"Type of object: {type(obj)}")
 
     # Отримуємо всі атрибути та методи об'єкта
     attributes_methods = dir(obj)
 
     # Виводимо атрибути та їх типи
-    print("\nАтрибути та методи:")
+    print("\nAttributes and methods:")
     for item in attributes_methods:
         value = getattr(obj, item, None)  # Отримуємо значення атрибута чи методу
         print(f"{item}: {type(value)}")  # Виводимо назву та тип атрибута чи методу
@@ -25,17 +22,13 @@ class MyClass:
 
     def __init__(self, value: str) -> None:
         """
-        Ініціалізує екземпляр MyClass з заданим значенням.
-
-        :param value: Значення, яке буде присвоєно атрибуту value.
+        Ініціалізує екземпляр MyClass з заданим значенням.(
         """
         self.value = value
 
     def say_hello(self) -> str:
         """
         Повертає привітальне повідомлення, яке включає атрибут value.
-
-        :return: Привітальне повідомлення.
         """
         return f"Hello, {self.value}"
 
