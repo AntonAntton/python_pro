bf4e12c98204f3eee122456e9f2e91bfe139fff7
AntonAntton <ivanko.anton1408@gmail.com>
2025-01-22 13:19:19 -0400

    Update homework_2_4.py

--- Lesson 2/homework_2_4.py
+++ Lesson 2/homework_2_4.py
@@ -3,10 +3,6 @@ from typing import Dict, Callable, Type
 def create_class(class_name: str, methods: Dict[str, Callable]) -> Type:
     """
     Динамічно створює новий клас з заданою назвою та методами.
-
-    :param class_name: Назва нового класу.
-    :param methods: Словник методів, де ключ — це назва методу, а значення — функція.
-    :return: Тип (клас) з динамічно доданими методами.
     """
     # Створюємо новий клас динамічно за допомогою type
     return type(class_name, (object,), methods)
@@ -14,16 +10,12 @@ def create_class(class_name: str, methods: Dict[str, Callable]) -> Type:
 def say_hello(self) -> str:
     """
     Повертає привітальне повідомлення.
-
-    :return: Привітальне повідомлення.
     """
     return "Hello!"
 
 def say_goodbye(self) -> str:
     """
     Повертає прощальне повідомлення.
-
-    :return: Прощальне повідомлення.
     """
     return "Goodbye!"
 
@@ -38,5 +30,5 @@ MyDynamicClass = create_class("MyDynamicClass", methods)
 
 # Приклад використання
 obj = MyDynamicClass()
-print(obj.say_hello())  # Hello!
-print(obj.say_goodbye())  # Goodbye!
+print(obj.say_hello())
+print(obj.say_goodbye())
