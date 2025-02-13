674aa80fc15c2792f8877150c953c299235428db
AntonAntton <ivanko.anton1408@gmail.com>
2025-02-13 15:14:00 -0400

    Update homework_2_2.py

--- homework_2/homework_2_2.py
+++ homework_2/homework_2_2.py
@@ -6,20 +6,12 @@ class Calculator:
     def add(self, a: int, b: int) -> int:
         """
         Додає два числа.
-
-        :param a: Перше число.
-        :param b: Друге число.
-        :return: Результат додавання.
         """
         return a + b
 
     def substract(self, a: int, b: int) -> int:
         """
         Віднімає одне число від іншого.
-
-        :param a: Перше число.
-        :param b: Друге число.
-        :return: Результат віднімання.
         """
         return a - b
 
@@ -27,11 +19,6 @@ class Calculator:
 def call_function(obj: object, method_name: str, *args: int) -> int:
     """
     Динамічно викликає метод об'єкта за його назвою та передає аргументи.
-
-    :param obj: Об'єкт, метод якого потрібно викликати.
-    :param method_name: Назва методу, який потрібно викликати.
-    :param args: Аргументи, які передаються методу.
-    :return: Результат виконання методу.
     """
     # Доступ до методу динамічно
     method = getattr(obj, method_name)
@@ -42,5 +29,5 @@ def call_function(obj: object, method_name: str, *args: int) -> int:
 
 # Приклад використання
 calc = Calculator()
-print(call_function(calc, "add", 10, 5))  # Виведе 15
-print(call_function(calc, "substract", 10, 5))  # Виведе 5
+print(call_function(calc, "add", 10, 5))
+print(call_function(calc, "substract", 10, 5))
